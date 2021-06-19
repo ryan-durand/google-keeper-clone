aab472dde1b8f5785447cf16f27640ab38042492
Ryan Durand <ryandurand70@gmail.com>
2021-06-19 18:42:08 -0400

    deploy react app to github pages

---
+++ README.md
@@ -0,0 +1,8 @@
+# google-keeper-clone
+
+# Usage Instructions
+npm install
+
+npm start
+
+A Google Keeper clone created in Code Sandbox with React
---
+++ build/precache-manifest.ad4d7f02373990060a45d8bd708c6cca.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "701d300f7a01b368138e1078d0fa43da",
+    "url": "/google-keeper-clone/index.html"
+  },
+  {
+    "revision": "7eb437abe843b795b5d2",
+    "url": "/google-keeper-clone/static/js/2.9cb32ddf.chunk.js"
+  },
+  {
+    "revision": "15e5a3e9be2b32ed6e27",
+    "url": "/google-keeper-clone/static/js/main.c2797198.chunk.js"
+  },
+  {
+    "revision": "b978fce822c863623ad6",
+    "url": "/google-keeper-clone/static/js/runtime-main.02be79b4.js"
+  }
+]);
